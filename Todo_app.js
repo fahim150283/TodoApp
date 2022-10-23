ca85eccf847ff40d750a532d2535305465edd22d
Abul Hossain Chowdhury <fahim150283@gmail.com>
2022-10-23 13:49:39 +0600

    dna

--- Todo_app.js
+++ Todo_app.js
@@ -1,3 +1,9 @@
 $("li").click(function(){
     $(this).toggleClass("completed");
 }); 
+
+// clickon X to delete the task
+$("span").click(function(event){
+    alert("click a span lol");
+    event.stopPropagation();
+});
