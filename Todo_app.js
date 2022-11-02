534af6640d7e180786e574896991144753ddc1fa
Abul Hossain Chowdhury <fahim150283@gmail.com>
2022-11-02 14:08:06 +0600

    updated

--- Todo_app.js
+++ Todo_app.js
@@ -4,6 +4,17 @@ $("li").click(function(){
 
 // clickon X to delete the task
 $("span").click(function(event){
-    alert("click a span lol");
+    // alert("click a span lol");
     event.stopPropagation();
+    $(this).parent().fadeOut(600, function(){//here (this)selects the X. parent is for the li and fadeout is for fading away slowly from the list
+        $(this).remove();
+    }); 
+});
+
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13) {
+        var todoText= $(this).val(); //it saves the input value to a variable
+        //create a new li and to ul
+    }
+    
 });
