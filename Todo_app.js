0bbe8639b10a496d1c860eff392a37f5e204d7cb
Abul Hossain Chowdhury <fahim150283@gmail.com>
2022-11-06 20:36:16 +0600

    2

--- Todo_app.js
+++ Todo_app.js
@@ -1,9 +1,9 @@
-$("li").click(function(){
+$("ul").on("click","li",function(){
     $(this).toggleClass("completed");
 }); 
 
 // clickon X to delete the task
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
     // alert("click a span lol");
     event.stopPropagation();
     $(this).parent().fadeOut(600, function(){//here (this)selects the X. parent is for the li and fadeout is for fading away slowly from the list
@@ -14,7 +14,9 @@ $("span").click(function(event){
 $("input[type='text']").keypress(function(event){
     if (event.which === 13) {
         var todoText= $(this).val(); //it saves the input value to a variable
+        $(this).val("");
         //create a new li and to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>")
     }
     
 });
